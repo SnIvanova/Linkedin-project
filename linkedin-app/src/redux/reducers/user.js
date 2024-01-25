2ef89d9ad67962306e34a64b9837d226a7ae4b7b
SnIvanova <1snizhanaivanova@gmail.com>
2024-01-25 17:45:50 +0100

    main

---
+++ linkedin-app/src/redux/reducers/user.js
@@ -0,0 +1,20 @@
+import { GET_USER } from '../actions'
+
+const initialState = {
+  userMe: [],
+}
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_USER:
+      return {
+        ...state,
+        userMe: action.payload,
+      }
+
+    default:
+      return state
+  }
+}
+
+export default userReducer
