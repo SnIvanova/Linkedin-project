8aa47e8ef0bb5b0670f2afdb5096f6c6f3fc758c
SnIvanova <1snizhanaivanova@gmail.com>
2024-01-29 23:30:11 +0100

    expreducer

--- linkedin-app/src/redux/store/index.js
+++ linkedin-app/src/redux/store/index.js
@@ -1,13 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from '../reducers/user';
 //import UserReducer from "../reducers/UserReducer";
-import ExperienceReducer from "../reducers/ExperienceReducer";
 import PostReducer from "../reducers/PostReducer";
 
 const  bigReducer = combineReducers({
   user: userReducer,
   //user: UserReducer,
-  experience: ExperienceReducer,
+  
   post: PostReducer,
 });
 
