e5c99ab11078a1b15d6ac14ff69e6e8f70b9d250
SnIvanova <1snizhanaivanova@gmail.com>
2024-01-29 23:40:20 +0100

    hope

--- linkedin-app/src/App.jsx
+++ linkedin-app/src/App.jsx
@@ -1,28 +1,24 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ProfilePage from './pages/ProfilePage'
-import UserProfile from "./components/UserProfile";
-
-
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import ProfilePage from './pages/ProfilePage';
+import NotFound from './pages/NotFound';
+import HomePage from './pages/HomePage';
+import MainProfile from './components/MainProfile';
 
 function App() {
   return (
-
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<ProfilePage />}/>
-        
-        <Route path="/profile/:userId" component={UserProfile} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route path='/*' element={<NotFound/>}/>
       </Routes>
-   
-      <Footer />
     </BrowserRouter>
-  )
-
+  );
 }
 
 export default App;
--- linkedin-app/src/components/MainProfile.jsx
+++ linkedin-app/src/components/MainProfile.jsx
@@ -2,51 +2,71 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Image } from 'react-bootstrap';
 import profile_img from '../assets/profile_img_PlaceHolder.png'
-import bg_img from '../assets/bg_img_placeHolder.png'
 import { Row, Col } from 'react-bootstrap'
 import { HiOutlinePencil } from "react-icons/hi2";
 import { BiCheckShield } from "react-icons/bi";
 import ProfileCarousel from './Profile/profileCarousel';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import EditProfileModal from './Profile/EditProfileModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserMe, getUserProfile } from '../redux/actions/user';
+import { useParams } from 'react-router-dom';
+
+
 
 export default function MainProfile() {
+
+  const { userId } = useParams()
   const [modalShow, setModalShow] = useState(false);
+  const dispatch = useDispatch()
+  const [edit, setEdit] = useState('')
+
+  const userMe = useSelector(state => state.user.userMe)
+
+  
+  useEffect(() => {
+
+    userId=== 'me' ?  dispatch(getUserMe()) :dispatch(getUserProfile(userId)) 
+
+  }, [userId])
+
+
 
   return (
 
     <>
-    <Card className='rounded-5'>
-      <div className='position-relative'>
-        <Card.Img variant='top' src={bg_img} alt='Background Image' height={350} />
-        <Row className="justify-content-start align-items-center position-absolute top-0 w-100">
-          <Col xs={12} className=" d-flex justify-content-end"><div className='bg-white rounded-circle p-2 m-4'><Image src={profile_img} height={20} width={20} /></div></Col>
-          <Col xs={12} className=' d-flex justify-content-start mt-5' ><div className='bg-white rounded-circle p-5 m-5'><Image src={profile_img} height={100} width={100} /></div></Col>
-        </Row>
-      </div>
-      <Card.Body >
-        <div className='d-flex justify-content-end'><Button variant='light' className='fs-3' type='button' onClick={() => {console.log('yerp');setModalShow(true)}}><HiOutlinePencil/></Button></div>
-        <Row>
-          <Col md={8}>
-            <Card.Title className='fs-1 d-inline-block m-0 me-2'>Name Namone </Card.Title>
-            <Button variant='outline-primary' className=' rounded-pill'><BiCheckShield className='fs-4'/>  Get verified</Button>
-            <Card.Text className='fw-bold'>Muratore presso muralandia </Card.Text>
-            <Card.Text className='text-muted mb-2'>Lagos, Nigeria, Africa <a variant='link' className='text-decoration-none'>Contact info</a></Card.Text>
-            <Card.Text className='text-primary my-1'>10 connections </Card.Text>
-            <Button variant="primary" className='rounded-pill py-2 px-3'>Open to</Button>
-            <Button variant="outline-primary" className='rounded-pill py-2 mx-3 px-3'>Add profile section</Button>
-            <Button variant="outline-dark" className='rounded-pill py-2 px-3'>More</Button>
-          </Col>
-          <Col className='d-flex '>
-            <img src={profile_img} alt="img" width={30} height={30} />
-            <Card.Text className='ms-1'>10 connections </Card.Text>
-          </Col>
-        </Row>
-      </Card.Body>
-        <ProfileCarousel />
-    </Card>
+      {userMe &&
+        <Card className='rounded-5'>
+          <div className='position-relative'>
+            <Card.Img variant='top' src={userMe.image} alt='Background Image' height={350} />
+            <Row className="justify-content-start align-items-center position-absolute top-0 w-100">
+              <Col xs={12} className=" d-flex justify-content-end"><div className='bg-white rounded-circle p-2 m-4'><Image src={profile_img} height={20} width={20} /></div></Col>
+              <Col xs={12} className=' d-flex justify-content-start mt-5 p-5' > <Image className='mt-5 rounded-circle border border-5' src={userMe.image} height={200} width={200} onClick={() => { setEdit('image'); setModalShow(true) }} /></Col>
+            </Row>
+          </div>
+          <Card.Body >
+            <div className='d-flex justify-content-end'><Button variant='light' className='fs-3' type='button' onClick={() => { setEdit('info'); setModalShow(true) }}><HiOutlinePencil /></Button></div>
+            <Row>
+              <Col md={8}>
+                <Card.Title className='fs-1 d-inline-block m-0 me-2'>{userMe.name} {userMe.surname} </Card.Title>
+                <Button variant='outline-primary' className=' rounded-pill'><BiCheckShield className='fs-4' />  Get verified</Button>
+                <Card.Text className='fw-bold'>{userMe.title} </Card.Text>
+                <Card.Text className='text-muted mb-2'>{userMe.area} <a variant='link' className='text-decoration-none'>Contact info</a></Card.Text>
+                <Card.Text className='text-primary my-1'> connections </Card.Text>
+                <Button variant="primary" className='rounded-pill py-2 px-3'>Open to</Button>
+                <Button variant="outline-primary" className='rounded-pill py-2 mx-3 px-3'>Add profile section</Button>
+                <Button variant="outline-dark" className='rounded-pill py-2 px-3'>More</Button>
+              </Col>
+              <Col className=' '>
+                <Card.Text className='ms-1'>About you: {userMe.bio} </Card.Text>
+              </Col>
+            </Row>
+          </Card.Body>
+          <ProfileCarousel />
+          <EditProfileModal show={modalShow} onHide={setModalShow} edit={edit} />
+        </Card>
+      }
 
-    <EditProfileModal show={modalShow} onHide={setModalShow}/>
     </>
   );
 }
